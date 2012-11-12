fbe82f43952dedabfe7ed8f81e06ae575701dcb0
Ciaran McCann <ciaran@flax.ie>
2012-11-12 14:01:17 +0000

    Added inital structure for the level, level will manage all the enities RWMPA-17

---
+++ js/Level.js
@@ -0,0 +1,16 @@
+var Level = (function(){
+
+	//Takes a level as a JSON string
+	function Level(mapJson){
+
+	}
+
+	Level.prototype.add = function(enity) {
+		
+	};
+
+	Level.prototype.update = function() {
+		
+	};
+
+})();
