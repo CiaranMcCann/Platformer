0b84f32a42ef002694d2311e80a94fa9287cce6d
unknown <dean__ryan@hotmail.com>
2012-11-12 16:43:08 +0000

    Worked on RWMPA-1 worked on basic level

--- js/Game.js
+++ js/Game.js
@@ -1,44 +1,33 @@
 var Game = (function () {
 
     function Game() {
- 
-        Graphics.init();
 
+        Graphics.init();
         this.canvas = Graphics.createCanvas("gameCanvas");
         this.canvasContext = this.canvas.getContext("2d");
-
-        Physics.init(this.canvas);    
+        Physics.init(this.canvas);
+        this.Level3 = new Level3();
+        this.Level3.add(new BreakableBody(100,500,100,100));
     }
 
-    Game.prototype.update = function () {
-      
-      //update code ...
-
+    Game.prototype.update = function () 
+    {
+        
     };
 
     Game.prototype.draw = function () {
-        
-      //draw code ...
-
-      var img = AssetManager.images["placeHolderImage"]; // Get my image from the asset manager
-
-      this.canvasContext.drawImage(
-        img,
-        this.canvas.width/2 - img.width/2,
-        this.canvas.height/2 - img.height/2
-      )
 
     };
 
     Game.prototype.step = function () {
-        
+
         Physics.world.Step(
-              (1 / 60)   
+              (1 / 60)
            , 10       //velocity iterations
            , 10       //position iterations
         );
-       Physics.world.DrawDebugData();
-       Physics.world.ClearForces();
+        Physics.world.DrawDebugData();
+        Physics.world.ClearForces();
 
     };
 
